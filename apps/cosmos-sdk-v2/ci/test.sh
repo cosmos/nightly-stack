7b61975311a8454682e966665be66b323f7f43ca
auricom <27022259+auricom@users.noreply.github.com>
2024-09-30 15:22:49 +0200

    debug

--- apps/cosmos-sdk-v2/ci/test.sh
+++ apps/cosmos-sdk-v2/ci/test.sh
@@ -3,13 +3,15 @@
 set -oue pipefail
 set -x
 
+BINARY_PATH=$(find $(go env GOPATH)/bin | grep $BINARY_NAME | tail -n 1)
+
 # Set the timeout to 60 seconds
 TIMEOUT=60
 START_TIME=$(date +%s)
 
 cd ${MATRIX_APP_REPOSITORY}
 make init-simapp-v2
-simdv2 start > ./output.log 2>1 &
+${BINARY_PATH} start > ./output.log 2>1 &
 APP_PID=$!
 
 
@@ -24,8 +26,9 @@ while true; do
     exit 1
   fi
 
+  check_height=$(curl --silent "http://localhost:26657/block?height=4" | jq --raw-output 'if .result.block.header.height then "Height exists: " + .result.block.header.height else "Height does not exist" end')
   # Check that 4th block is produced to validate the application
-  if simd query block-results 4; then
+  if [[ -z "$check_height" && "$check_height" == "Height exists: 4" ]]; then
     echo "Block #4 has been committed. Application is working correctly."
     kill $APP_PID
     exit 0
