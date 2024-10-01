106b32bb3708033502e742f6e3c613c8ad022130
auricom <27022259+auricom@users.noreply.github.com>
2024-10-01 14:53:34 +0200

    build deps

--- apps/cosmos-sdk-v2/entrypoint.sh
+++ apps/cosmos-sdk-v2/entrypoint.sh
@@ -4,16 +4,16 @@ set -Eeuo pipefail
 set -x
 
 COSMOS_CHAIN_ID=${COSMOS_CHAIN_ID:-testchain}
-COSMOS_MONIKER=${COSMOS_MONIKER:-node}
-COSMOS_NODE_HOME=${COSMOS_NODE_HOME:-/config}
-COSMOS_START_CMD=${COSMOS_START_CMD:-start}
+COSMOS_MONIKER=${COSMOS_MONIKER:-testchain-node}
+COSMOS_NODE_HOME=${NODE_HOME:-/config}
+COSMOS_START_CMD=${START_CMD:-start}
 
-if [[ ! -f "${COSMOS_NODE_HOME}/config.json" ]]; then
+if [[ ! -f "${COSMOS_NODE_HOME}/config/config.toml" ]]; then
     echo "Launch init procedure..."
     /app/node config set client chain-id ${COSMOS_CHAIN_ID} --home ${COSMOS_NODE_HOME}
     /app/node config set client keyring-backend test --home ${COSMOS_NODE_HOME}
-    /app/node keys add alice --home ${COSMOS_NODE_HOME}
-    /app/node keys add bob --home ${COSMOS_NODE_HOME}
+    /app/node keys add alice --indiscreet --home ${COSMOS_NODE_HOME}
+    /app/node keys add bob --indiscreet --home ${COSMOS_NODE_HOME}
     /app/node init ${COSMOS_MONIKER} --chain-id ${COSMOS_CHAIN_ID} --home ${COSMOS_NODE_HOME}
     jq '.app_state.gov.params.voting_period = "600s"' ${COSMOS_NODE_HOME}/config/genesis.json > temp.json && mv temp.json ${COSMOS_NODE_HOME}/config/genesis.json
     jq '.app_state.gov.params.expedited_voting_period = "300s"' ${COSMOS_NODE_HOME}/config/genesis.json > temp.json && mv temp.json ${COSMOS_NODE_HOME}/config/genesis.json
