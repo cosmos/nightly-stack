c081fb1b02baf3a3e20b1984afddbc463435d91d
auricom <27022259+auricom@users.noreply.github.com>
2024-10-04 10:57:27 +0200

    fixup! fix: cosmos-sdk-v2 build flags

--- apps/cosmos-sdk-v2/ci/build.sh
+++ apps/cosmos-sdk-v2/ci/build.sh
@@ -3,10 +3,19 @@
 set -Eeuo pipefail
 set -x
 
+if [[ ${GOARCH} == "arm64" ]]; then
+    export CC=aarch64-linux-gnu-gcc
+    export CGO_ENABLED=1
+    export GOOS=linux
+fi
+
+# Install CORSS tools
+sudo apt install gcc-aarch64-linux-gnu
+
 # Import go modules
 cd ${MATRIX_APP_REPOSITORY}/${MATRIX_APP_PATH}
 go mod tidy
 
 # Build application
 cd ..
-CGO_ENABLED=1 COSMOS_BUILD_OPTIONS=v2 make install
+COSMOS_BUILD_OPTIONS=v2 make install
