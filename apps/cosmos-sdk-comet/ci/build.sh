2fccd9fa77cc2299ff316df2bccfc004b1c1de6b
auricom <27022259+auricom@users.noreply.github.com>
2025-01-13 11:33:34 +0100

    fix: migration build

--- apps/cosmos-sdk-comet/ci/build.sh
+++ apps/cosmos-sdk-comet/ci/build.sh
@@ -7,9 +7,6 @@ set -x
 cd ${MATRIX_APP_REPOSITORY}/${MATRIX_APP_PATH}
 go mod tidy
 
-upgrade_name="v050-to-v052"
-sed -i "/app\.UpgradeKeeper = upgradekeeper\.NewKeeper/ a \\\tapp.upgradeKeeper.SetUpgradeHandler(\"${upgrade_name}\", func(ctx sdk.Context, plan upgrade.Plan) {}" ~/cosmos-sdk/simapp/app.go
-
 # Build application
 cd ../..
 make install
