1b7bde1c67e0d5b9fdd4a5304e5f5f255fbc5cc3
auricom <27022259+auricom@users.noreply.github.com>
2025-02-03 09:30:08 +0100

    feat(wasmd): Update wasmvm to 2.2.1

--- apps/cosmos-sdk-comet/ci/build.sh
+++ apps/cosmos-sdk-comet/ci/build.sh
@@ -3,12 +3,6 @@
 set -Eeuo pipefail
 set -x
 
-# Check if the architecture is arm64
-if [ "$(uname -m)" = "aarch64" ]; then
-    # Install the required packages
-    sudo apt-get install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu
-fi
-
 # Import go modules
 cd ${MATRIX_APP_REPOSITORY}/${MATRIX_APP_PATH}
 go mod tidy
