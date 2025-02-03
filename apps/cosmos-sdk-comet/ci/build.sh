aa4e29c6ca28a4a49d3b84027c08678fc8fbbba0
auricom <27022259+auricom@users.noreply.github.com>
2025-02-03 09:25:26 +0100

    fix: cosmos-sdk-comet

--- apps/cosmos-sdk-comet/ci/build.sh
+++ apps/cosmos-sdk-comet/ci/build.sh
@@ -3,10 +3,16 @@
 set -Eeuo pipefail
 set -x
 
+# Check if the architecture is arm64
+if [ "$(uname -m)" = "aarch64" ]; then
+    # Install the required packages
+    sudo apt-get install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu
+fi
+
 # Import go modules
 cd ${MATRIX_APP_REPOSITORY}/${MATRIX_APP_PATH}
 go mod tidy
 
 # Build application
 cd ../..
-make install
+COSMOS_BUILD_OPTIONS=sqlite make install
